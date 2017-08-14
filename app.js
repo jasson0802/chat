784f1e3f494ecc7d51d4d2c3e2aa3b1b9563081e
Jasson Gonzalez <jasson0802@gmail.com>
2017-08-13 18:21:28 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app        = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+//http://localhost:8080/{endpoint}
+
+router.get('/', function(req, res) {
+    res.json({ mensaje: 'Funciona' });   
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Servidor corriendo en puerto: ' + port);
